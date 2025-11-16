1fca7fee2d12eaa1f1220537d52042eabab77dc5
Simon Fuller <simon_fuller@outlook.com>
2025-11-16 16:17:02 +0000

    rename app component

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,17 @@
+import { TestBed } from '@angular/core/testing';
+
+import { App } from './app.component';
+
+describe('App', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [App],
+    }).compileComponents();
+  });
+
+  test('should create the app', () => {
+    const fixture = TestBed.createComponent(App);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+});
